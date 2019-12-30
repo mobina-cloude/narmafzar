eb47341100b10061e017d6d9aadbbca8863ae763
Mahnaz <mahnaz.divargar@gmail.com>
2019-12-30 02:41:27 -0800

    file added

---
+++ pr/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+
+
+        {
+            singleton.Instance.message();
+            singleton.Instance.message();
+            singleton.Instance.message();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pr/singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr
+{
+    class singleton
+    {
+       
+            private static singleton  instance = null;
+            public static singleton Instance
+        { get
+            { if (instance == null)
+                    instance = new singleton();
+                return instance;
+
+                    }
+        }
+
+
+
+        private singleton()
+        {
+            Console.WriteLine("object printed");
+        }
+        public void message()
+        {
+            Console.WriteLine("ok");
+        }
+        
+
+    
+    }
+}
